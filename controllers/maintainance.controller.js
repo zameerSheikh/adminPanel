f70e6f17d2ed34145c27fc4629f8a858ef36d90c
Zameer <zameer@ahex.co.in>
2019-01-22 19:19:01 +0530

    maintainance mode implementation

--- controllers/maintainance.controller.js
+++ controllers/maintainance.controller.js
@@ -5,14 +5,40 @@
 
     maintainanceController.$inject = [
         '$scope',
-        '$timeout'
+        '$timeout',
+        'LoginService',
+        'AdminAppService'
     ]
 
     function maintainanceController(
         $scope,
-        $timeout
+        $timeout,
+        LoginService,
+        AdminAppService
     ){
 
+        $scope.maintainance = {
+            isMaintainanceEnabled: false,
+            changeMode: changeMode
+        };
+
+        function changeMode(isEnabled){
+            if(isEnabled){
+                LoginService.enableMaintainance().then(function(response){
+                    console.log('response: ', response);
+                    AdminAppService.showSuccessMessage(response.data.data.message);
+                }).catch(function(error){
+                    console.log('error: ', error);
+                });
+            }else{
+                LoginService.disableMaintainance().then(function(response){
+                    console.log('response: ', response);
+                    AdminAppService.showSuccessMessage(response.data.data.message);
+                }).catch(function(error){
+                    console.log('error: ', error);
+                });
+            }
+        }
     }
     
-}())
+}());
--- services/login.service.js
+++ services/login.service.js
@@ -22,11 +22,88 @@
         return {
             loginAdmin: loginAdmin,
             getUserList: getUserList,
-            getDomainsList: getDomainsList
+            getDomainsList: getDomainsList,
+            enableMaintainance: enableMaintainance,
+            disableMaintainance: disableMaintainance
+        };
+
+
+        /*
+         * @author: Zameer
+         * @created: 18-10-2016
+         * @params: User credentials(object)
+         * @return: success, error functions
+         * @purpose: to enable maintainance mode
+         */
+        function enableMaintainance(){
+            var apiUrl = ADMIN_ENGAGETO_API_URL + 'admin/' + $localStorage.adminId + '/application/down';
+            var request = $http({
+                method: 'POST',
+                url: apiUrl,
+                headers: {
+                    'Authorization': 'Bearer ' + $localStorage.token,
+                }
+            });
+
+            return(request
+                    .then(enableMaintainanceSuccess)
+                    .catch(enableMaintainanceError));
+
+
+            // success function
+            function enableMaintainanceSuccess(response){
+                return(response);
+            }
+
+            // error function
+            function enableMaintainanceError(response){
+                if (!angular.isObject(response.data) || !response.data.message) {
+                    return($q.reject(response.data));
+                }
+                // Otherwise, use expected error message.
+                return($q.reject(response.data.message));
+            }
         }
 
         /*
-         * @author: sandeep
+         * @author: Zameer
+         * @created: 18-10-2016
+         * @params: User credentials(object)
+         * @return: success, error functions
+         * @purpose: to disable maintainance mode
+         */
+        function disableMaintainance(){
+            var apiUrl = ADMIN_ENGAGETO_API_URL + 'admin/' + $localStorage.adminId + '/application/up';
+            var request = $http({
+                method: 'POST',
+                url: apiUrl,
+                headers: {
+                    'Authorization': 'Bearer ' + $localStorage.token,
+                }
+            });
+
+            return(request
+                    .then(disableMaintainanceSuccess)
+                    .catch(disableMaintainanceError));
+
+
+            // success function
+            function disableMaintainanceSuccess(response){
+                return(response);
+            }
+
+            // error function
+            function disableMaintainanceError(response){
+                if (!angular.isObject(response.data) || !response.data.message) {
+                    return($q.reject(response.data));
+                }
+                // Otherwise, use expected error message.
+                return($q.reject(response.data.message));
+            }
+        }
+
+        /*
+         * @author: Zameer
          * @created: 18-10-2016
          * @params: User credentials(object)
          * @return: success, error functions
@@ -59,7 +136,7 @@
         }
 
         /*
-         * @author: sandeep
+         * @author: Zameer
          * @created: 18-10-2016
          * @params: User credentials(object)
          * @return: success, error functions
@@ -94,7 +171,7 @@
         }
 
         /*
-         * @author: sandeep
+         * @author: Zameer
          * @created: 18-10-2016
          * @params: User credentials(object)
          * @return: success, error functions
